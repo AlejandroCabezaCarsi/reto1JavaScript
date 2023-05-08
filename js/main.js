5b75a56921ae5c2b5cb24e2f12bb2acbfb59b6c6
Alex <cabezacarsialejandro@gmail.com>
2023-05-08 16:45:57 +0200

    Práctica acabada

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+let num1 = 10; 
+let num2 = 10; 
+
+if (+num1 > +num2){ 
+    console.log(+num1 + ' es mayor que ' + +num2);
+} if (+num2 > +num1) {
+    console.log(+num2 + ' es mayor que ' + +num1);
+} if (+num1 == +num2){
+    console.log('Los números son iguales')
+}
